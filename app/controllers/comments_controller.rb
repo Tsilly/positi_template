599287b8017ebe61e050a301d16a9129c9bea57b
Jill Li <jill.li87@gmail.com>
2015-08-31 22:30:57 +0200

    comment is nested under post

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.new(params.require(:comment).permit(:body))
+		
+		
+		if @comment.save
+			flash[:notice] = "Your comment is sent."
+			redirect_to post_path(@post)
+		else
+			render 'posts/show'
+		end
+	end
+
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update]
+	
+	def index
+		@posts = Post.all
+	end
+
+	def show 
+		@comment = @post.comments.new
+		
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(params_post)
+		if @post.save
+			flash[:notice] = "Your post is saved."
+			redirect_to posts_path
+		else
+			render :new
+		end
+	end
+
+	def edit ;end
+
+	def update
+		if @post.update(params_post)
+			flash[:notice] = "Your post is updated."
+			redirect_to post_path(@post)
+		else
+			render :edit
+		end
+	end
+
+	private
+		def params_post
+			params.require(:post).permit(:title, :url, :description)
+		end
+
+		def find_post
+			@post = Post.find(params[:id])
+		end
+end
